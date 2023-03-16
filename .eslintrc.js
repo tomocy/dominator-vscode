d4cd2b2ce4d75d9c173369760305834e3c1ff630
tomocy <tomocy.dev@gmail.com>
2023-03-16 15:51:22 +0900

    Use Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,12 @@ module.exports = {
     browser: false,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
